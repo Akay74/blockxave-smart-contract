c9f534bab0dc3ad11184f2af94eb5bc391a1138d
ejim favour <favourejim56@gmail.com>
2023-08-22 22:00:45 +0100

    initaited contract and added git ignore

---
+++ contracts/Savings.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Idenditifier: MIT
+pragma solidity ^0.8.4;
+
+//Connect user wallet to dApp
+//Allow user create and customise saving plan
+//Allow user deploy a savings contract
+//Allow user approve the contract to take and store funds for a particular amount of time
+//Display user funds when they connect to the dApp after funding
+//Allow user to withdraw funds from immediate savings
+//Deposit funds back to user when time for saving elapses and also send incentive token
+//Allow user to stake stable tokens for monthly or yearly time period
+//Deposit funds to user address after staking time elapses
+
+contract Savings {
+  string private immutable i_savingsName;
+  address public s_owner;
+
+  constructor(string memory _savingsName) {
+    i_savingsName = _savingsName;
+    s_owner = msg.sender;
+  }
+
+  function getSavingsName() public view returns (string memory) {
+    return i_savingsName;
+  }
+}
