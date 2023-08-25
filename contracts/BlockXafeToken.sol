ff26420464d6f8bd836444988429317f7a1431ff
ejim favour <favourejim56@gmail.com>
2023-08-25 08:39:23 +0100

    modified the token contract

--- contracts/BlockXafeToken.sol
+++ contracts/BlockXafeToken.sol
@@ -5,11 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BlockXafeToken is ERC20, Ownable {
+
     constructor(uint256 _initialSupply) ERC20("Blockxafe-Token", "BXT"){
         _mint(msg.sender, _initialSupply);
     }
 
-    function incentivizeSavers(address saver, uint256 amount) external onlyOwner {
-        _mint(saver, amount);
+    function mintNewTokens(uint _amount)external onlyOwner{
+        _mint(msg.sender, _amount);
     }
 }
