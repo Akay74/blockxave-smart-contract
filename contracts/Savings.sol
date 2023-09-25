20169bd3f8fcc7a2293cbae11b747d90c93ad721
ejim favour <favourejim56@gmail.com>
2023-09-25 07:28:15 +0100

    removed unused imports and comments

--- contracts/Savings.sol
+++ contracts/Savings.sol
@@ -6,14 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-// user connects wallet
-// user deploys savings contract with the savings name and the chosen stable token
-// when deployed, smart contract automatically creates a general savings
-// user can deposit into the savings contract
-// user can then create a new savings box
-// user can deposit into the newly created savings box and set a  timer
-// user can see the total savings
-
 error Savings__UnlockTimeNotReached();
 error Savings__DepositFailed();
 error Savings__TransferFailed();
@@ -35,20 +27,12 @@ contract Savings is Ownable {
     address private immutable i_owner;
     mapping(uint256 => SavingPlan) private s_idToSavingPlan;
     uint256 private s_savingPlansCounter;
-    uint256 public constant DEPLOY_FEE = 1 * 10 ** 18;
-    address private constant BLOCKXAVE_ADDRESS = 0x6fb462259dEE0956FfAE3a87C8481885c2db34bB;
 
     event FundsDesposited(address indexed saver, uint256 amount);
     event FundsWithdrawn(address indexed saver, uint256 amount);
 
     constructor(string memory _savingsName, address _stableTokenAddress) {
         s_savingsName = _savingsName;
-
-        SavingPlan memory generalSavings = SavingPlan("General savings", 0, 0, block.timestamp);
-        s_idToSavingPlan[s_savingPlansCounter] = generalSavings;
-
-        s_savingPlansCounter += 1;
-
         i_owner = msg.sender;
         s_stableToken = IERC20(_stableTokenAddress);
     }
@@ -58,7 +42,7 @@ contract Savings is Ownable {
         uint256 _amount,
         uint256 _target,
         uint256 _unlockTime
-    ) external onlyOwner {
+    ) external onlyOwner returns (uint256) {
         s_stableToken.transferFrom(i_owner, address(this), _amount);
 
         uint256 unlockTime = block.timestamp + (_unlockTime * 1 days);
@@ -66,6 +50,12 @@ contract Savings is Ownable {
         SavingPlan memory savingPlan = SavingPlan(_savingsPlanName, _amount, _target, unlockTime);
         s_idToSavingPlan[s_savingPlansCounter] = savingPlan;
         s_savingPlansCounter += 1;
+
+        if (s_savingPlansCounter == 0) {
+            return s_savingPlansCounter;
+        } else {
+            return s_savingPlansCounter - 1;
+        }
     }
 
     //  deposit
@@ -97,19 +87,7 @@ contract Savings is Ownable {
         savingPlan.target = 0;
         savingPlan.unlockTime = 0;
 
-        // emit FundsWithdrawn(i_owner, savingPlan.total);
-    }
-
-    function withdrawFromGeneralSavings(uint256 _amount) external onlyOwner {
-        SavingPlan storage savingPlan = s_idToSavingPlan[0];
-
-        //    transfer the saved money from the contract to the saver
-        bool callSuccess = s_stableToken.transfer(i_owner, _amount);
-        if (!callSuccess) revert Savings__TransferFailed();
-
-        savingPlan.total -= _amount;
-        savingPlan.target = 0;
-        savingPlan.unlockTime = 0;
+        emit FundsWithdrawn(i_owner, savingPlan.total);
     }
 
     function getSavingPlanName(uint256 id) public view returns (string memory) {
