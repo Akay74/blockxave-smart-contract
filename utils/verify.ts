461a4b92099744c9889907dce6d7fd0db3c14696
ejim favour <favourejim56@gmail.com>
2023-09-27 09:29:57 +0100

    added a contract arguement is the verify function

--- utils/verify.ts
+++ utils/verify.ts
@@ -1,11 +1,15 @@
 import { run } from "hardhat"
 
-export const verify = async function (contractAddress: string, args: string[]): Promise<void> {
+export const verify = async function (
+    contract: string,
+    contractAddress: string,
+    args: string[]
+): Promise<void> {
     try {
         await run("verify:verify", {
             address: contractAddress,
             constructorArguments: args,
-            contract: "contracts/BlockXafeToken.sol:BlockXafeToken",
+            contract: contract,
         })
     } catch (err: any) {
         if (err.message.toLowerCase().includes("already verified")) {
