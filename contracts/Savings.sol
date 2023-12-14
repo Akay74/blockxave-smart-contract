b02fe2cab83fd54455d632b5205f7a58e4c07e8a
ejim favour <favourejim56@gmail.com>
2023-12-14 23:44:25 +0100

    modified the create saving plan function to be able to create fixed plans

--- contracts/Savings.sol
+++ contracts/Savings.sol
@@ -4,20 +4,26 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// user actions
+// deploy savings contract
+// create a saving plan
+// deposit into the saving plan
+// withdraw from the saving plan
 
-    error Savings__UnlockTimeNotReached();
-    error Savings__DepositFailed();
-    error Savings__TransferFailed();
+error Savings__UnlockTimeNotReached();
+error Savings__DepositFailed();
+error Savings__TransferFailed();
 
 /*
-* @contract Savings Contract
-* @author Ejim Favour
-*/
+ * @contract Savings Contract
+ * @author Ejim Favour
+ */
 
 contract Savings is Ownable {
     // Type Declarations
     struct SavingPlan {
         string savingPlanName;
+        bool fixedPlan;
         uint256 total;
         uint256 target;
         uint256 unlockTime;
@@ -34,8 +40,8 @@ contract Savings is Ownable {
     event FundsWithdrawn(address indexed saver, uint256 amount);
 
     /*
-    * @param _savingsName and _stableTokenAddress
-    */
+     * @param _savingsName and _stableTokenAddress
+     */
     constructor(string memory _savingsName, address _stableTokenAddress) {
         s_savingsName = _savingsName;
         i_owner = msg.sender;
@@ -53,11 +59,12 @@ contract Savings is Ownable {
     }
 
     /*
-    * @param _savingsPlanName, _amount. _target, _unlockTime
-    * @func it is a function for creating a saving plan
-    */
+     * @param _savingsPlanName, _amount. _target, _unlockTime
+     * @func it is a function for creating a saving plan
+     */
     function createSavingPlan(
         string memory _savingsPlanName,
+        bool _fixedPlan,
         uint256 _amount,
         uint256 _target,
         uint256 _unlockTime
@@ -68,15 +75,21 @@ contract Savings is Ownable {
 
         uint256 unlockTime = block.timestamp + (_unlockTime * 1 days);
 
-        SavingPlan memory savingPlan = SavingPlan(_savingsPlanName, _amount, _target, unlockTime);
+        SavingPlan memory savingPlan = SavingPlan(
+            _savingsPlanName,
+            _fixedPlan,
+            _amount,
+            _target,
+            unlockTime
+        );
         s_idToSavingPlan[s_savingPlansCounter] = savingPlan;
         s_savingPlansCounter += 1;
     }
 
     /*
-    * @param id and _amount
-    * @func it is a function for depositing into a saving plan
-    */
+     * @param id and _amount
+     * @func it is a function for depositing into a saving plan
+     */
     //  deposit
     function deposit(uint256 id, uint256 _amount) external onlyOwner {
         //    transfer the savings money from the saver to the contract
@@ -92,9 +105,9 @@ contract Savings is Ownable {
     }
 
     /*
-    * @param _id
-    * @func it is a function for withdrawing from a saving plan
-    */
+     * @param _id
+     * @func it is a function for withdrawing from a saving plan
+     */
     function withdrawFromSavingPlan(uint256 _id) external onlyOwner {
         SavingPlan storage savingPlan = s_idToSavingPlan[_id];
 
